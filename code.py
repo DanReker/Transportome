f2e819e52e9de4a64daf8861dda3ad05ff35e002
DanReker <56095399+DanReker@users.noreply.github.com>
2022-07-21 15:42:03 -0400

    Updated code.py
    Updated code to Py3, included imbalanced learn controls, added additional machine learning pipeline steps (missing values, standardization, variance filter)

--- code.py
+++ code.py
@@ -19,12 +19,21 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.cluster import KMeans
+# data processing
+from imblearn import under_sampling, over_sampling
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import VarianceThreshold
+from imblearn.pipeline import Pipeline
 # machine learning metrics
+import sklearn
 from sklearn.metrics import matthews_corrcoef as mcc
 from sklearn.metrics import accuracy_score as accuracy
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import balanced_accuracy_score as bac
 from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.metrics import roc_auc_score as rocauc
+from imblearn.metrics import sensitivity_specificity_support 
 # machine learning utility functions
 from sklearn.model_selection import LeaveOneGroupOut
 from sklearn.model_selection import cross_val_predict
@@ -55,8 +64,8 @@ def read_mols(filename, idrow=False):
 	smis = []
 	ids = []
 	
-	infile = open(filename,"rb")
-	infile.next()
+	infile = open(filename,"r")
+	next(infile)
 	
 	for line in infile:
 		nam = line.split("\t")[0]
@@ -72,7 +81,7 @@ def read_mols(filename, idrow=False):
 				ids += [transid(line.split("\t")[-1].strip())]
 		
 		else:
-			print "error " + line.split("\t")[3] + " " + line.split("\t")[4]
+			print ("error " + line.split("\t")[3] + " " + line.split("\t")[4])
 			None
 		
 	
@@ -94,7 +103,7 @@ def describe_mol(mol):
 		if v > np.finfo(np.float32).max: 	# postprocess descriptors for freak large values
 			ds_n.append(np.finfo(np.float32).max)
 		elif math.isnan(v):
-			ds_n.append(np.float32(0.0))
+			ds_n.append(np.nan)
 		else:
 			ds_n.append(np.float32(v))
 	
@@ -110,12 +119,15 @@ def describe_mols(mols):
 
 ########
 # function to standardize calculations of benchmark statistics
-def evaluate(pred_vals,true_vals):
+def evaluate(pred_vals,true_vals,pred_prob):
 	tn, fp, fn, tp = confusion_matrix(true_vals, pred_vals).ravel()
+	sensitivity, specificity, _ = sensitivity_specificity_support(true_vals, pred_vals,labels=[1],average='binary')
 	return [mcc(true_vals,pred_vals),
 	accuracy(true_vals,pred_vals),
 	bac(true_vals,pred_vals),
-	tn, fp, fn, tp]
+	tn, fp, fn, tp,
+	sensitivity, specificity,
+	rocauc(true_vals,pred_prob)]
 	
 
 
@@ -132,9 +144,11 @@ x = np.array(describe_mols(train[1]))
 y = np.array(train[2])
 m = np.array(train[1])
 
-models = [ RandomForestClassifier(n_estimators=500), GaussianNB(),KNeighborsClassifier(3), DecisionTreeClassifier(),MLPClassifier(),LinearSVC(),ExtraTreesClassifier(), GaussianProcessClassifier()]
-model_comparision=[]
+models = [ RandomForestClassifier(n_estimators=500), GaussianNB(),KNeighborsClassifier(3), DecisionTreeClassifier(),MLPClassifier(),LinearSVC(),ExtraTreesClassifier()]
+model_comparison=[]
 for model in models:
+	
+	pipe = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()), ('classification', model)])
 	
 	for i in range(3):
 		mask = np.array([a is not None for a in y[:,i]])
@@ -143,25 +157,50 @@ for model in models:
 		
 		temp = []
 		for ii in range(5):
-			preds = sklearn.model_selection.cross_val_predict(model,x_sub,y_sub,cv=10,n_jobs=-1)
-			temp += [evaluate(preds,y_sub)]
+			pred_prob = sklearn.model_selection.cross_val_predict(pipe,x_sub,y_sub,cv=10,n_jobs=-1,method='predict_proba')[:,1]
+			preds = pred_prob > 0.5
+			temp += [evaluate(preds,y_sub,pred_prob)]
 	
-		model_comparision+= [temp]
+		model_comparison+= [temp]
 
 
-model_comparision = np.array(model_comparision)
+model_comparison = np.array(model_comparison)
 
-for i in range(len(model_comparision)):
-	print "\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(model_comparision[i,:],axis=0),3),np.round(np.std(model_comparision[i,:],axis=0),3))])
+for i in range(len(model_comparison)):
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(model_comparison[i,:],axis=0),2),np.round(np.std(model_comparison[i,:],axis=0),3))]))
 
 
+########
+# ROC curve plotting
+from sklearn.metrics import roc_curve
+import pylab as pl
 
+models = [ RandomForestClassifier(n_estimators=500), GaussianNB(),KNeighborsClassifier(3), DecisionTreeClassifier(),MLPClassifier(),LinearSVC(),ExtraTreesClassifier()]
+
+for model in models:
+	
+	pipe = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()), ('classification', model)])
+	
+	for i in range(3):
+		mask = np.array([a is not None for a in y[:,i]])
+		y_sub = y[mask,i].astype(int)
+		x_sub = x[mask]
+		
+		pred_prob = sklearn.model_selection.cross_val_predict(pipe,x_sub,y_sub,cv=10,n_jobs=-1,method='predict_proba')[:,1]
+		
+		fpr, tpr, _ = roc_curve(y_sub, pred_prob)
+		pl.plot(fpr,tpr)
+
+pl.show()
 
 ########
 # leave cluster out validation based on MACCS similarity clusters
 maccs_k = [MACCSkeys.GenMACCSKeys(mol) for mol in m]
 kmeans = KMeans(n_clusters=50, random_state=0).fit(maccs_k)
 maccs_cluster = kmeans.predict(maccs_k)
+
+model = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500,class_weight="balanced"))])
+
 cluster_performance = []
 for i in range(3):
 	temp = []
@@ -172,15 +211,99 @@ for i in range(3):
 	c_sub = maccs_cluster[mask]
 	
 	for ii in range(5):
-		preds = cross_val_predict(model,x_sub,y_sub,groups=c_sub,cv=LeaveOneGroupOut(),n_jobs=-1)
-		temp += [evaluate(preds,y_sub)]
+		pred_prob = cross_val_predict(model,x_sub,y_sub,groups=c_sub,cv=LeaveOneGroupOut(),n_jobs=-1,method='predict_proba')[:,1]
+		preds = pred_prob > 0.5
+		temp += [evaluate(preds,y_sub,pred_prob)]
 	
 	cluster_performance += [temp]
 
 cluster_performance = np.array(cluster_performance)
 
 for i in range(len(cluster_performance)):
-	print "\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(cluster_performance[i,:],axis=0),3),np.round(np.std(cluster_performance[i,:],axis=0),3))])
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(cluster_performance[i,:],axis=0),3),np.round(np.std(cluster_performance[i,:],axis=0),3))]))
+
+
+
+
+
+#########
+# examine potential for imblearn improvements through upsampling and downsampling
+unsample = [under_sampling.AllKNN(), under_sampling.ClusterCentroids(), under_sampling.CondensedNearestNeighbour(), under_sampling.EditedNearestNeighbours(), under_sampling.InstanceHardnessThreshold(), under_sampling.NearMiss(), under_sampling.NeighbourhoodCleaningRule(), under_sampling.OneSidedSelection(), under_sampling.RandomUnderSampler(), under_sampling.RepeatedEditedNearestNeighbours()]
+ovsample = [over_sampling.ADASYN(), over_sampling.BorderlineSMOTE(), over_sampling.RandomOverSampler(), over_sampling.SMOTE(), over_sampling.SMOTEN(), over_sampling.SVMSMOTE()]
+
+models = [ Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('sampling', samp),('classification', RandomForestClassifier(n_estimators=500))]) for samp in unsample + ovsample]
+
+model_comparison = []
+for model in models:
+	
+	for i in range(3):
+		mask = np.array([a is not None for a in y[:,i]])
+		y_sub = y[mask,i].astype(int)
+		x_sub = x[mask]
+		
+		temp = []
+		for ii in range(5):
+			pred_prob = sklearn.model_selection.cross_val_predict(model,x_sub,y_sub,cv=10,n_jobs=-1,method='predict_proba')[:,1]
+			preds = pred_prob > 0.5
+			temp += [evaluate(preds,y_sub,pred_prob)]
+	
+		model_comparison+= [temp]
+
+
+model_comparison = np.array(model_comparison)
+
+for i in range(len(model_comparison)):
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(model_comparison[i,:],axis=0),3),np.round(np.std(model_comparison[i,:],axis=0),3))]))
+
+#########
+# examine potential for weighting to improve predictions
+
+model = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500,class_weight="balanced"))])
+
+model_comparison_w = []
+
+for i in range(3):
+	mask = np.array([a is not None for a in y[:,i]])
+	y_sub = y[mask,i].astype(int)
+	x_sub = x[mask]
+	
+	temp = []
+	for ii in range(5):
+		pred_prob = sklearn.model_selection.cross_val_predict(model,x_sub,y_sub,cv=10,n_jobs=-1,method='predict_proba')[:,1]
+		preds = pred_prob > 0.5
+		temp += [evaluate(preds,y_sub,pred_prob)]
+	
+	model_comparison_w += [temp]
+
+model_comparison_w = np.array(model_comparison_w)
+
+for i in range(len(model_comparison_w)):
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(model_comparison_w[i,:],axis=0),3),np.round(np.std(model_comparison_w[i,:],axis=0),3))]))
+
+#########
+# test adversarial controls
+
+model = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500))])
+
+adv_control = []
+for i in range(3):
+	mask = np.array([a is not None for a in y[:,i]])
+	y_sub = y[mask,i].astype(int)
+	y_shuffle = np.random.permutation(y_sub)
+	x_sub = x[mask]
+	
+	temp = []
+	for ii in range(5):
+		preds = sklearn.model_selection.cross_val_predict(model,x_sub,y_shuffle,cv=10,n_jobs=-1)
+		temp += [evaluate(preds,y_shuffle)]
+	
+	adv_control += [temp]
+
+
+
+for i in range(len(adv_control)):
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(adv_control[i,:],axis=0),3),np.round(np.std(adv_control[i,:],axis=0),3))]))
+
 
 
 ########
@@ -205,7 +328,7 @@ for i in range(3):
 	conf1  += model.predict_proba(x[mask2])[:,1].tolist()
 
 
-outfile = open("predictions_for_training","wb")
+outfile = open("predictions_for_training","w")
 for i in range(len(preds1)):
 	outfile.write("\t".join([trans1[i], names1[i], smis1[i], str(preds1[i]), str(conf1[i])]) + "\n")
 
@@ -224,7 +347,7 @@ lowconf_total= 0
 
 for transp in ["mdr1","bcrp","mrp2"]:
 	infile = open("additional_" + transp + ".tsv")
-	_ = infile.next()
+	_ = infile.readline() # skip header
 	
 	scores = []
 	clas = []
@@ -253,9 +376,9 @@ for transp in ["mdr1","bcrp","mrp2"]:
 
 
 
-print highconf_true * 1.0 / highconf_total
+print (highconf_true * 1.0 / highconf_total)
 # 0.838709677419
-print lowconf_true * 1.0 / lowconf_total
+print (lowconf_true * 1.0 / lowconf_total)
 # 0.521739130435
 
 
@@ -268,7 +391,7 @@ num_annotations = [49, 34, 20]
 for i in range(3):
 	cnt = 0
 	infile = open("additional_" + transporters[i] + ".tsv","r")
-	_ = infile.next()
+	_ = infile.readline() # skip header
 	for line in infile:
 		name = line.split("\t")[0]
 		smi = line.split("\t")[1]
@@ -287,7 +410,7 @@ for i in range(3):
 ########
 # semi-supervised matrix completion
 
-model = RandomForestClassifier(n_estimators=500)
+model = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500))])
 
 y_augm = np.array([[None,None,None] for i in range(len(y2))])
 
@@ -301,13 +424,14 @@ for i in range(3):
 	model.fit(x_sub,y_sub)
 	
 	y_augm[mask,i] = y_sub
- 	y_augm[mask2,i] = model.predict(x[mask2])
+	y_augm[mask2,i] = model.predict(x[mask2])
+	
 	assert all(a is not None for a in y_augm[:,i])
 
 
 types = [[1,1,1],[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[0,0,0]]
 for t in types:
-	print str(t) + "\t" + str(np.sum([all(a == t) for a in y_augm]))
+	print (str(t) + "\t" + str(np.sum([all(a == t) for a in y_augm])))
 
 
 n_drugbank = np.loadtxt("database_drugbank5_only.tsv",delimiter="\t",usecols=(0,),dtype=object)
@@ -318,17 +442,32 @@ sum(np.sum(drugbank_augm,axis=1) > 1) *1.0 / len(drugbank_augm)
 
 types = [[1,1,1],[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[0,0,0]]
 for t in types:
-	print str(t) + "\t" + str(np.sum([all(a == t) for a in drugbank_augm]))
+	print (str(t) + "\t" + str(np.sum([all(a == t) for a in drugbank_augm])))
+
+types = [[1,1,1],[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[0,0,0]]
+for t in types:
+	print (str(t) + "\t" + str(np.sum([all(a == t) for a in y_augm])))
+
+
+n_drugbank = np.loadtxt("database_drugbank5_only.tsv",delimiter="\t",usecols=(0,),dtype=object)
+drugbank_mask = np.array([name in n_drugbank for name in n])
+drugbank_augm = y_augm[drugbank_mask]
+
+sum(np.sum(drugbank_augm,axis=1) > 1) *1.0 / len(drugbank_augm)
+
+types = [[1,1,1],[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[0,0,0]]
+for t in types:
+	print (str(t) + "\t" + str(np.sum([all(a == t) for a in drugbank_augm])))
 
 
 ########
 # predict model drug library
 
 
-model = RandomForestClassifier(n_estimators=500)
+model = Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500))])
 
 infile = open("drug_library.tsv")
-_ = infile.next()
+_ = infile.readline() # skip header
 model_drugs_smiles = []
 model_drugs_profiles = []
 for line in infile:
@@ -354,7 +493,7 @@ correct_preds = 0
 for pred, prof in zip(model_drugs_preds,model_drugs_profiles):
 	correct_preds += np.sum(pred == prof)
 
-print "correct preds: " + str(correct_preds) + " ( " + str(float(correct_preds) / (3*28)) + " % )"
+print ("correct preds: " + str(correct_preds) + " ( " + str(float(correct_preds) / (3*28)) + " % )")
 
 
 
@@ -390,7 +529,7 @@ x_investigational = []
 profile_investigational = []
 
 infile = open("investigational_compounds.tsv","r")
-_ = infile.next()
+_ =  infile.readline() # skip header
 for line in infile:
 	temp = line.split("\t")
 	if not Chem.MolFromSmiles(temp[1]) is None:
@@ -421,7 +560,7 @@ conf_matrix = np.transpose(prediction_confidences)
 names_investigational = np.array(names_investigational)
 
 for ideal in [[1,1,1],[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[0,0,0]]:
-	outfile = open("top15_" + str(ideal) + ".txt","w")
+	outfile = open("temp_top15_" + str(ideal) + ".txt","w")
 	for name in names_investigational[np.argsort([np.sum(np.abs(c-ideal)) for c in conf_matrix])][:15]:
 		outfile.write(name +"\n")
     
@@ -456,7 +595,7 @@ for i in range(len(investigational_library[0])):
 
 ####
 # print sizes of datasets
-print "\t\tPgp\tBCRP\tMRP2"
+print ("\t\tPgp\tBCRP\tMRP2")
 for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 	print ("Substrates\t"+"\t".join([str(_n) for _n in np.sum(yi==True,axis=0)]))
 	print ("Non-Substrates\t"+"\t".join([str(_n) for _n in np.sum(yi==False,axis=0)]))
@@ -466,7 +605,7 @@ for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 ########
 # compare models based on cross validation performance
 
-model = RandomForestClassifier(n_estimators=500,oob_score=True)
+model =  Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500))])
 cross_perfs = []
 for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 	preds = np.array([])
@@ -479,15 +618,17 @@ for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 		
 		temp = []
 		for ii in range(5):
-			preds = cross_val_predict(model,x_sub,y_sub,n_jobs=-1)
-			temp += [evaluate(preds,y_sub)]
+			pred_prob = sklearn.model_selection.cross_val_predict(model,x_sub,y_sub,cv=10,n_jobs=-1,method='predict_proba')[:,1]
+			preds = pred_prob > 0.5
+			temp += [evaluate(preds,y_sub,pred_prob)]
+		
 		
 		cross_perfs += [temp]
 
 cross_perfs = np.array(cross_perfs)
 
 for i in range(12):
-	print "\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(cross_perfs[i,:],axis=0),3),np.round(np.std(cross_perfs[i,:],axis=0),3))])
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(cross_perfs[i,:],axis=0),3),np.round(np.std(cross_perfs[i,:],axis=0),3))]))
 
 
 for i in range(1,4):
@@ -496,7 +637,8 @@ for i in range(1,4):
 ########
 # compare models based on oob performance
 
-model = RandomForestClassifier(n_estimators=500,oob_score=True)
+
+model =  Pipeline([('missing_values', SimpleImputer() ), ('feature_selection',VarianceThreshold()), ('scaler', StandardScaler()),('classification', RandomForestClassifier(n_estimators=500,oob_score=True))])
 oob_perfs = []
 for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 	t_x = np.array([])
@@ -512,10 +654,11 @@ for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 			
 			
 			_ = model.fit(x_sub,y_sub)
-			preds = np.argmax(model.oob_decision_function_,axis=1)
-			#preds_proba = model.oob_decision_function_[:,1]
-			temp += [evaluate(preds,y_sub)]
-	
+			
+			preds_proba = model[-1].oob_decision_function_[:,1]
+			preds = preds_proba > 0.5
+			temp += [evaluate(preds,y_sub,preds_proba)]
+		
 		oob_perfs += [temp]	
 
 
@@ -523,12 +666,14 @@ for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 oob_perfs = np.array(oob_perfs)
 
 for i in range(12):
-	print "\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(oob_perfs[i,:],axis=0),3),np.round(np.std(oob_perfs[i,:],axis=0),3))])
+	print ("\t".join([str(a) + "±" + str(b) for a,b in zip(np.round(np.mean(oob_perfs[i,:],axis=0),3),np.round(np.std(oob_perfs[i,:],axis=0),3))]))
+
+
 
 
 
 ########
-# compare models based on oob performance and high confidence predictions
+# compare models based on high confidence OOB predictions
 
 model = RandomForestClassifier(n_estimators=50,oob_score=True)
 for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
@@ -572,6 +717,6 @@ for (xi,yi) in [(x,y),(x,y2),(x3,y3),(x4,y4)]:
 probas = np.array(probas)
 
 for p in probas:
-	print str(float(np.sum(p<0.2) + np.sum(p>0.8)))
+	print (str(int(np.sum(p<0.2) + np.sum(p>0.8))))
 
 
